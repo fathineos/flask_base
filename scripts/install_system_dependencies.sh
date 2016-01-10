0c8403ee6ff9d5c068b17ce0d2918a52db929633
Fotis Athineos <athinaios.fotios@gmail.com>
2016-01-10 16:21:59 +0200

    modifing install_system_dependencies to work with docker

--- scripts/install_system_dependencies.sh
+++ scripts/install_system_dependencies.sh
@@ -1,6 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ $(uname) == "Linux" ]]; then
-	# TODO either  make generic for all linuxes or remove
-	sudo apt-get install python-virtualenv python-pip libmysqlclient-dev
+    if [ ${DOCKER} ]; then
+        apt-get install -qq -y --force-yes python-virtualenv python-pip libmysqlclient-dev python-dev
+    else
+        sudo apt-get install -qq -y --force-yes python-virtualenv python-pip libmysqlclient-dev python-dev
+    fi
+elif [[ $(uname) == "Darwin" ]]; then
+    if ! command -v "brew" >/dev/null 2>&1 ; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew install python
+    sudo pip install virtualenv
 fi
+exit 0
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name="base",
       packages=find_packages(),
-      version="0.1.30",
+      version="0.1.31",
       description="flask framework utility library",
       author="Fotis Athinaios",
       author_email="fotis.athineos@gmail.com",
